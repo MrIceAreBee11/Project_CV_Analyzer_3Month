89ea6acb7e25cdb4b1a6eb588c4de4780cb5bd7f
unknown <rayyis@student.telkomuniversity.ac.id>
2026-03-30 11:23:57 +0700

    add cv-analyzer folder

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print(1+2)
+
+i = 2
+while i < 100:
+    j = 2
+    while j <= (i / j):
+        if not (i % j):
+            break
+        j = j + 1
+
+    if j > (i / j):
+        print(i, "is prime")
+
+    i = i + 1
+
+print("Good bye!")
